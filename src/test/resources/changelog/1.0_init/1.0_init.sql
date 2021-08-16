b3f9ad6371d308dc482d2fee8d37847070b498d2
Alexandr tataritsky <luckonly@gmail.com>
2021-08-16 16:56:43 +0300

    Docker settings fixes

--- src/test/resources/changelog/1.0_init/1.0_init.sql
+++ src/test/resources/changelog/1.0_init/1.0_init.sql
@@ -1,6 +1,4 @@
-CREATE SCHEMA if not exists simpleWebApp;
-
-CREATE TABLE IF NOT EXISTS simpleWebApp.employees
+CREATE TABLE IF NOT EXISTS employee
 (
     employee_id bigserial primary key,
     first_name varchar(100) not null,
@@ -12,15 +10,15 @@ CREATE TABLE IF NOT EXISTS simpleWebApp.employees
     deleted BOOLEAN
 );
 
-INSERT INTO simpleWebApp.employees(first_name, last_name, department_id, job_title, gender, date_of_birth, deleted)
+INSERT INTO employee(first_name, last_name, department_id, job_title, gender, date_of_birth, deleted)
 VALUES ('Alexey', 'Ivanov', '12', 'Content manager', 'MALE', '19900410', '0');
-INSERT INTO simpleWebApp.employees(first_name, last_name, department_id, job_title, gender, date_of_birth, deleted)
+INSERT INTO employee(first_name, last_name, department_id, job_title, gender, date_of_birth, deleted)
 VALUES ('Ivan', 'Petrov', '11', 'Business analytic', 'MALE', '19900912', '0');
-INSERT INTO simpleWebApp.employees(first_name, last_name, department_id, job_title, gender, date_of_birth, deleted)
+INSERT INTO employee(first_name, last_name, department_id, job_title, gender, date_of_birth, deleted)
 VALUES ('Dmitry', 'Maksimov', '10', 'Developer', 'MALE', '19790611', '0');
-INSERT INTO simpleWebApp.employees(first_name, last_name, department_id, job_title, gender, date_of_birth, deleted)
+INSERT INTO employee(first_name, last_name, department_id, job_title, gender, date_of_birth, deleted)
 VALUES ('Elena', 'Dmitrieva', '9', 'Marketing', 'FEMALE', '19850424', '0');
-INSERT INTO simpleWebApp.employees(first_name, last_name, department_id, job_title, gender, date_of_birth, deleted)
+INSERT INTO employee(first_name, last_name, department_id, job_title, gender, date_of_birth, deleted)
 VALUES ('Natalya', 'Petrova', '10', 'Developer', 'FEMALE', '19910414', '0');
-INSERT INTO simpleWebApp.employees(first_name, last_name, department_id, job_title, gender, date_of_birth, deleted)
+INSERT INTO employee(first_name, last_name, department_id, job_title, gender, date_of_birth, deleted)
 VALUES ('Olga', 'Ivanova', '11', 'Product manager', 'FEMALE', '19890408', '0');
